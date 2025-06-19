0ad59a9c8ea9e14efc41b20efa292877400df52e
mml1020 <mario@mario.com>
2025-06-19 16:23:44 +0200

    Refactorización: reemplazar tipo código con patrón State/Strategy

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -17,19 +17,31 @@ public class Movie {
 	public static final int NEW_RELEASE = 1;
 
 	private String _title;
-	private int _priceCode;
+	private Price _price;
 
 	public Movie(String title, int priceCode) {
 		_title = title;
-		_priceCode = priceCode;
+		setPriceCode(priceCode);
 	}
 
 	public int getPriceCode() {
-		return _priceCode;
+		return _price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		_priceCode = arg;
+		switch (arg) {
+		case REGULAR:
+			_price = new Regular();
+			break;
+		case CHILDRENS:
+			_price = new Children();
+			break;
+		case NEW_RELEASE:
+			_price = new NewRelease();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
 	}
 
 	public String getTitle() {
@@ -37,31 +49,10 @@ public class Movie {
 	}
 	
 	public double getCharge(int daysRented) {
-		double thisAmount = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (daysRented > 2)
-				thisAmount += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (daysRented > 3)
-				thisAmount += (daysRented - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return _price.getCharge(daysRented);
 	}
 	
 	public int getFrequentRenterPoints(int daysRented) {
-		int frequentRenterPoints = 1;
-		// add bonus for a two day new release rental
-		if ((getPriceCode() == Movie.NEW_RELEASE)
-				&& daysRented > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
+		return _price.getFrequentRenterPoints(daysRented);
 	}
 }
