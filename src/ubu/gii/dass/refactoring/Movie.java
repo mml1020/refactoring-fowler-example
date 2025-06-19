80bcf9883f626290f38e3d8de64987b446330163
mml1020 <mario@mario.com>
2025-06-19 16:15:30 +0200

    Refactorización: mover lógica de cálculo de Movie desde Rental

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -35,4 +35,33 @@ public class Movie {
 	public String getTitle() {
 		return _title;
 	}
+	
+	public double getCharge(int daysRented) {
+		double thisAmount = 0;
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (daysRented > 2)
+				thisAmount += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (daysRented > 3)
+				thisAmount += (daysRented - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
+	
+	public int getFrequentRenterPoints(int daysRented) {
+		int frequentRenterPoints = 1;
+		// add bonus for a two day new release rental
+		if ((getPriceCode() == Movie.NEW_RELEASE)
+				&& daysRented > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
 }
